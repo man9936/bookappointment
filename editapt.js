b92f28f662599de03883d2ef15ffb51f92cfc7be
man9936 <112785474+man9936@users.noreply.github.com>
2022-09-27 15:26:16 +0530

    Create editapt.js

---
+++ editapt.js
@@ -0,0 +1,174 @@
+ 
+// Book Class: Represents a Book
+class Book {
+    constructor(name, phone, dateTime) {
+      this.name = name;
+      this.phone = phone;
+      this.dateTime = dateTime;
+    }
+  }
+  
+  // UI Class: Handle UI Tasks
+  class UI {
+    static displayBooks() {
+      const books = Store.getBooks();
+  
+      books.forEach((book) => UI.addBookToList(book));
+    }
+  
+    static addBookToList(book) {
+      const list = document.querySelector('#book-list');
+  
+      const row = document.createElement('tr');
+  
+      row.innerHTML = `
+        <td>${book.name}</td>
+        <td>${book.phone}</td>
+        <td>${book.dateTime}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        <td><a href="#" class="btn btn-toolbar btn-sm " >Edit</a></td>
+        
+      `;
+  
+      list.appendChild(row);
+    }
+  
+    static deleteBook(el) {
+      if(el.classList.contains('delete')) {
+        el.parentElement.parentElement.remove();
+      }
+    }
+
+////edit button
+    static editbooking(name,phone,dateTime){
+        
+         
+         
+          document.querySelector('#name').value=name;
+        
+      document.querySelector('#phone').value=phone;
+      document.querySelector('#date').value=dateTime;
+           
+        
+          
+       
+                
+        }
+       
+          
+
+        
+        
+
+
+    
+  
+    static showAlert(message, className) {
+      const div = document.createElement('div');
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+      const container = document.querySelector('.container');
+      const form = document.querySelector('#book-form');
+      container.insertBefore(div, form);
+  
+      // Vanish in 3 seconds
+      setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+  
+    static clearFields() {
+      document.querySelector('#name').value = '';
+      document.querySelector('#phone').value = '';
+      document.querySelector('#date').value = '';
+    }
+  }
+  
+  // Store Class: Handles Storage
+  class Store {
+    static getBooks() {
+      let books;
+      if(localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+  
+      return books;
+    }
+  
+    static addBook(book) {
+      const books = Store.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  
+    static removeBook(dateTime) {
+      const books = Store.getBooks();
+  
+      books.forEach((book, index) => {
+        if(book.dateTime === dateTime) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
+  
+  // Event: Display Books
+  document.addEventListener('DOMContentLoaded', UI.displayBooks);
+  
+  // Event: Add a Book
+  document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+  
+    // Get form values
+    const name = document.querySelector('#name').value;
+    const phone = document.querySelector('#phone').value;
+    const dateTime = document.querySelector('#date').value;
+  
+    // Validate
+    if(name === '' || phone === '' || dateTime === '') {
+      UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+      // Instatiate book
+      const book = new Book(name, phone, dateTime);
+  
+      // Add Book to UI
+      UI.addBookToList(book);
+  
+      // Add book to store
+      Store.addBook(book);
+  
+      // Show success message
+      UI.showAlert('Appointment Added', 'success');
+  
+      // Clear fields
+      UI.clearFields();
+    }
+  });
+  
+  // Event: Remove a Book
+  document.querySelector('#book-list').addEventListener('click', (e) => {
+    // Remove book from UI
+    UI.deleteBook(e.target);
+  
+    // Remove book from store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+  
+    // Show success message
+    UI.showAlert('Appointment Removed', 'success');
+
+    //edit
+   
+  });
+
+  document.querySelector("#book-list").addEventListener('click',(e) =>{
+    if(e.target.classList==="btn btn-toolbar btn-sm "){
+      UI.deleteBook(e.target);
+      Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+      UI.editbooking(e.target.parentElement.parentElement);
+      }
+  });
+
+ 
